49d4e38750ad820cba38e25d89f00de7185d5dc0
Jose <josemauricio2211@gmail.com>
2022-08-01 12:52:05 -0400

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_24_220942) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_164604) do
   create_table "adding_products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_24_220942) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
